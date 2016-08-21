1f938a6aa433c2955fa1adf59f992ff0238d9417
Jesus Imery <yisas_0929@hotmail.com>
2016-08-21 15:48:22 -0400

    tato is jumping

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -10,16 +10,36 @@ public class CharacterController : MonoBehaviour {
 	[HideInInspector]
 	public bool jump = false;				// Condition for whether the player should jump.
 
-	public float speed;						// Player speed
+	public float maxSpeed = 5f;			    // As fast as player can go
 	public float moveForce = 365f;			// Amount of force added to move the player left and right.
+	public float jumpForce = 1000f;			// Amount of force added when the player jumps.
+
+	private bool isGrounded = false;        // Bool for checking if player is grounded, uses 
+	private Transform groundCheck;			// A position marking where to check if the player is grounded.
+	private Animator anim;					// Reference to the player's animator component.
+
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+	void Awake()
+	{
+		// Setting up references.
+		groundCheck = transform.Find("groundCheck");
+		anim = GetComponent<Animator>();
+	}
+
 	// Update is called once per frame
 	void Update () {
+
+		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+		isGrounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
+
+		// If the jump button is pressed and the player is grounded then the player should jump.
+		if(Input.GetButtonDown("Jump") && isGrounded)
+			jump = true;
 	
 	}
 
@@ -29,11 +49,18 @@ public class CharacterController : MonoBehaviour {
 		// Cache the horizontal input.
 		float h = Input.GetAxis("Horizontal");
 
-		// Move
-		if(h * GetComponent<Rigidbody2D>().velocity.x < 0)
+		// The Speed animator parameter is set to the absolute value of the horizontal input.
+		anim.SetFloat("Speed", Mathf.Abs(h));
+
+		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+		if(h * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
 			// ... add a force to the player.
 			GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
 
+		// If the player's horizontal velocity is greater than the maxSpeed...
+		if(Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
+			// ... set the player's velocity to the maxSpeed in the x axis.
+			GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
 		// If the input is moving the player right and the player is facing left...
 		if(h > 0 && !facingRight)
@@ -43,6 +70,11 @@ public class CharacterController : MonoBehaviour {
 		else if(h < 0 && facingRight)
 			// ... flip the player.
 			FlipCharacter();
+
+		// Jump if conditions are met, checks happen in update
+		if (jump)
+			Jump ();
+
 	}
 
 	void FlipCharacter() {
@@ -54,5 +86,15 @@ public class CharacterController : MonoBehaviour {
 		Vector3 tempScale = transform.localScale;
 		tempScale.x *= -1;
 		transform.localScale = tempScale;
+	}
+
+	void Jump(){
+		
+		// Add a vertical force to the player.
+		GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+
+		// Make sure the player can't jump again until the jump conditions from Update are satisfied.
+		jump = false;
+
 	}
 }
