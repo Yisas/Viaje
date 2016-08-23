e24084071de4e0371f9a05ed64005f62ecf397e0
Jesus Imery <yisas_0929@hotmail.com>
2016-08-23 13:31:17 -0400

    ranged animation tato, initial r[Aanged weapon script

---
+++ Assets/Scripts/WeaponRanged.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponRanged : MonoBehaviour {
+
+	private PlayerController playerController;
+	private Transform shotSpawn;							// Transform of empty object where the shot is initialized
+
+	void Awake(){
+		//Set up references
+		playerController= GetComponentInParent<PlayerController>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D (Collision2D col){
+
+		if (col.gameObject.tag == "Enemy") {
+			col.gameObject.GetComponent<EnemyController> ().Die ();
+			playerController.MeleeHit ();
+		}
+	}
+}
