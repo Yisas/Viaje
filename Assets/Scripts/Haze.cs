d01b54e74902937a32234be99386fcbf8512528c
Jesus Imery <yisas_0929@hotmail.com>
2016-08-25 18:04:03 -0400

    haze effect collider triggers and countdown time

--- Assets/Scripts/Haze.cs
+++ Assets/Scripts/Haze.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class Haze : MonoBehaviour {
 
+	public float hazePeriod;					// Amount of time the haze effect is visible on screen.
+
+	private Canvas hazeCanvas;
 	private SpriteRenderer hazeSprite;
+	private float timer = 0;					// Countdown timer for haze effect being visible on screen
+	private bool isDisplaying = false;
 
 	void Awake(){
-		hazeSprite = GetComponentInChildren<SpriteRenderer> ();
+		hazeCanvas = GameObject.FindGameObjectWithTag ("HazeEffectCanvas").GetComponent<Canvas>();
+		hazeSprite = hazeCanvas.GetComponentInChildren<SpriteRenderer> ();
 	}
 
 
@@ -17,6 +23,18 @@ public class Haze : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (isDisplaying && (Time.time - timer) >= hazePeriod) {
+				timer = 0;
+				hazeSprite.enabled = false;
+				isDisplaying = false;
+			}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.transform.tag == "Player") {
+			timer = Time.time;
+			hazeSprite.enabled = true;
+			isDisplaying = true;
+		}
 	}
 }
