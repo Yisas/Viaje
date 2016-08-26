5329e417c0b3ee141f043aafc87bb6a8dcfd7e3b
Jesus Imery <yisas_0929@hotmail.com>
2016-08-26 03:02:32 -0400

    burger drop animation an initial scripting

---
+++ Assets/Scripts/HealthDrop.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HealthDrop : MonoBehaviour {
+
+	private Animator anim;
+
+	void Awake(){
+		//Set up references
+		anim = GetComponent<Animator>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		// On collision with ground deactivate disposable landing gear. Layer 8 should be Ground
+		if (col.gameObject.layer == 8) {
+			//landingGear.ForEach (DeactivateSprite);
+			anim.SetTrigger("landed");
+		}
+	}
+}
+			
