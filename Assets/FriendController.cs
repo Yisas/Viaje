e726dd3d5f91c1ea9cad43db6f6cfeb092efc683
Jesus Imery <yisas_0929@hotmail.com>
2016-08-28 00:45:11 -0400

    friend scene loader

---
+++ Assets/FriendController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class FriendController : MonoBehaviour {
+
+	public bool isSceneChanger;
+	public string leveName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.transform.tag == "Player")
+			SceneManager.LoadScene (leveName);
+	}
+}
