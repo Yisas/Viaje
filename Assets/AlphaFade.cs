10d656b00879dddbe7708cd688652882b3a8225a
Jesus Imery <yisas_0929@hotmail.com>
2016-08-28 00:17:20 -0400

    sidehow is working

---
+++ Assets/AlphaFade.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlphaFade : MonoBehaviour {
+
+	public float minimum = 0.0f;
+	public float maximum = 1f;
+	public float displayedDuration = 2f;
+	//[HideInInspector]
+	public bool start = false;
+
+	private float duration;									// Inherit from parent slideshow.
+	private float timer = 0;
+	private SpriteRenderer sprite;
+	private float transitionTime;
+	private bool started = false;
+
+	void Start() {
+		sprite = GetComponent<SpriteRenderer> ();
+		duration = GetComponentInParent<Slideshow> ().slideshowInterval;
+		transitionTime = (duration / 2) - (displayedDuration / 2);
+	}
+
+	void Update() {
+
+		if (start) {
+			timer = duration;
+			start = false;
+			started = true;
+		}
+
+		//Debug.Log (timer);
+		if (started) {
+			// Before waiting interval
+			if (timer <= transitionTime)
+				sprite.color = new Color (1f, 1f, 1f, Mathf.SmoothStep (minimum, maximum, timer / transitionTime));
+			// After waiting interval
+			else if (timer > transitionTime + displayedDuration){
+				sprite.color = new Color (1f, 1f, 1f, Mathf.SmoothStep (maximum, minimum, (timer - transitionTime - displayedDuration) / transitionTime));  
+			}
+
+			timer -= Time.deltaTime;
+
+			if (sprite.enabled == false)
+				sprite.enabled = true;
+		}
+	}
+
+	public void Reset(){
+		started = false;
+	}
+}
---
+++ Assets/Slideshow.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Slideshow : MonoBehaviour {
+
+	public float slideshowInterval;
+
+	private SpriteRenderer[] pictures;
+	private float slideshowTimer;
+	private int currentTextureIndex = 0;
+
+	void Awake(){
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		pictures = transform.GetComponentsInChildren<SpriteRenderer> ();
+		slideshowTimer = slideshowInterval;
+		StartDisplay (currentTextureIndex);
+		currentTextureIndex++;
+		Mathf.Clamp (currentTextureIndex, 0, pictures.Length-1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		slideshowTimer -= Time.deltaTime;
+		if (slideshowTimer <= 0) {
+			slideshowTimer = 0;
+			StopDisplay(currentTextureIndex);
+			currentTextureIndex++;
+			if (currentTextureIndex > pictures.Length - 1)
+				currentTextureIndex = 0;
+			StartDisplay(currentTextureIndex);
+			slideshowTimer = slideshowInterval;
+		}
+	}
+
+	private void StopDisplay(int index){
+		pictures[index].enabled = false;
+		pictures[index].GetComponent<AlphaFade> ().Reset ();
+	}
+
+	private void StartDisplay(int index){
+		pictures[index].GetComponent<AlphaFade> ().start = true;
+	}
+}
