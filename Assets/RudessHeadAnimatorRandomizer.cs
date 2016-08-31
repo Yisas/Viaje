48c39193488ac30e615435a0559ab02b4cdc6429
Jesus Imery <yisas_0929@hotmail.com>
2016-08-31 06:13:46 -0400

    rudess head random animations

---
+++ Assets/RudessHeadAnimatorRandomizer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RudessHeadAnimatorRandomizer : MonoBehaviour {
+
+	public float blinkMinInterval;
+	public float blinkMaxInterval;
+	public float smileMinInterval;
+	public float smileMaxInterval;
+
+	private Animator anim;
+	private bool counting = false;
+	private bool animationType = false;						// Blink is true, smile is false
+	private float countdownTimer;
+
+
+	void Awake(){
+		anim = GetComponentInChildren<Animator> ();
+	}
+
+	void Update(){
+		if (counting) {
+			countdownTimer -= Time.deltaTime;
+			if (countdownTimer <= 0) {
+				if (animationType)
+					anim.SetTrigger ("blink");
+				else
+					anim.SetTrigger ("smile");
+				
+				counting = false;
+			}
+		} else
+			ChooseAnimation ();
+	}
+
+
+	void ChooseAnimation(){
+		if (Random.value <= 0.5f) 
+			animationType = true;
+
+						if (animationType)
+			countdownTimer = Random.Range (blinkMinInterval, blinkMaxInterval);
+		else
+			countdownTimer = Random.Range (smileMinInterval, smileMaxInterval);
+		
+		counting = true;
+	}
+}
