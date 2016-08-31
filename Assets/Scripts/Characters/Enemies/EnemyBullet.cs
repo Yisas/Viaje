4292b251d96161d77e17ea10adeab05f6fd8f7ea
Jesus Imery <yisas_0929@hotmail.com>
2016-08-31 02:30:34 -0400

    flying head rudess is done, needs proper spawner effect for lightning

--- Assets/Scripts/Characters/Enemies/EnemyBullet.cs
+++ Assets/Scripts/Characters/Enemies/EnemyBullet.cs
@@ -5,15 +5,22 @@ public class EnemyBullet : Bullet {
 
 	private PlayerHealth playerHealth;
 
-	void Awake(){
+	protected void Awake(){
 		playerHealth = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerHealth> ();
 	}
 
-	void Update(){
+	protected void Update(){
 		base.Update ();
 	}
 
 	new void OnCollisionEnter2D (Collision2D col){
+		if (col.transform.tag == "Player") {
+			playerHealth.TakeDamage (transform);
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
 		if (col.transform.tag == "Player") {
 			playerHealth.TakeDamage (transform);
 			Destroy (this.gameObject);
---
+++ Assets/Scripts/Characters/Enemies/SpawningEnemyBullet.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawningEnemyBullet : EnemyBullet {
+
+	public GameObject spawnEffect;
+	public float spawnEffectInterval;
+	public float preDamageInterval;
+
+	private float spawnEffectTimer;
+	private float preDamageTimer;
+	private bool hasSpawned = false;
+	private bool startDamage = false;
+
+	void Awake(){
+		base.Awake ();
+		spawnEffectTimer = spawnEffectInterval;
+		preDamageTimer = preDamageInterval;
+	}
+
+	void Start(){
+		GameObject portal = (GameObject) Instantiate (spawnEffect, transform.position, transform.rotation);
+		portal.GetComponent<ParticleSystem> ().startLifetime = spawnEffectInterval;
+		portal.GetComponent<ParticleSystem> ().Play ();
+	}
+
+	new void Update(){
+		spawnEffectTimer -= Time.deltaTime;
+
+		if (spawnEffectTimer <= 0 && !hasSpawned) {
+			GetComponent<SpriteRenderer> ().enabled = true;
+			hasSpawned = true;
+		}
+
+		if (hasSpawned)
+			preDamageTimer -= Time.deltaTime;
+
+		if (preDamageTimer <= 0 && !startDamage) {
+			startDamage = true;
+			GetComponent<BoxCollider2D> ().enabled = true;
+		}
+
+		if (startDamage)
+			base.Update ();
+	}
+}
