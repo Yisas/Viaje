af3593f179e240fb480ba4eec4633be8e38e83cf
Jesus Imery <yisas_0929@hotmail.com>
2016-09-03 12:16:32 -0400

    initial loading screen work

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour {
+
+	private float timer;
+	private bool loading = false;
+
+	// Use this for initialization
+	void Start () {
+		timer = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer <= 0 && !loading) {
+			loading = true;
+			SceneManager.LoadScene ("Llovizna");
+		}
+	}
+}
