a8498001a29479eb9e9865a915f4ee2b4d560136
Jesus Imery <yisas_0929@hotmail.com>
2016-09-06 07:33:34 -0400

    more work on initial house scene

---
+++ Assets/SceneSwitch.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour {
+
+	public string sceneName;
+
+	public void SwitchScene(){
+		GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ().nextLevelName = sceneName;
+		SceneManager.LoadScene ("LoadingScreen");
+	}
+}
