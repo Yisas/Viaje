936e068d39cb70bc253b8153985dfec1eef27f70
Yisas <yisas_0929@hotmail.com>
2017-08-18 00:27:38 -0400

    Final cutscene setup

--- Assets/Scripts/Characters/Enemies/EnemyController.cs
+++ Assets/Scripts/Characters/Enemies/EnemyController.cs
@@ -1,77 +1,103 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class EnemyController : MonoBehaviour
 {
 
-	public float moveSpeed = 2f;
-	// The speed the enemy moves at.
-	public float deathDespawnInterval;
-	// Time the character is on the scene after dying.
-	public float headbobSpeedMultiplier;
-	// To be passed to animator.
+    public float moveSpeed = 2f;
+    // The speed the enemy moves at.
+    public float deathDespawnInterval;
+    // Time the character is on the scene after dying.
+    public float headbobSpeedMultiplier;
+    // To be passed to animator.
 
-	private float deathTimer = 0f;
-	// When timer reaches despawn time, destroy object.
-	protected bool isDead = false;
-	// Check for whether enemy is dead, despawn after interval.
-	protected Animator anim;
-	// Reference to the enemy's animator component.
-	protected Transform frontCheck;
-	// Reference to the position of the gameobject used for checking if something is in front.
-	private GameController gameController;
-	private PlayerController playerController;
-	private bool isFacingLeft = true;
-	private bool isGrounded = false;						// Check is now using OnCollisionExit with Ground layer mask
-	private bool hasEnemyBelow = false;						// See EnemyBelow() function
-	private float dismountTime = 3f;						// Wait time when stacked on top of another enemy to unstack
-	private float dismountTimer = 0;						// Countdown initially set to 0
-	protected bool lockMovement = false;
+    public AudioClip[] idleClips;
+    public float idleSoundCountdown;                        // Time until next clip
+    public float idleSoundRandom;                           // +- range for a random clip after countdown
+
+    private float idleSoundTimer = 0f;
+    private float deathTimer = 0f;
+    // When timer reaches despawn time, destroy object.
+    protected bool isDead = false;
+    // Check for whether enemy is dead, despawn after interval.
+    protected Animator anim;
+    // Reference to the enemy's animator component.
+    protected Transform frontCheck;
+    // Reference to the position of the gameobject used for checking if something is in front.
+    private GameController gameController;
+    private PlayerController playerController;
+    private AudioSource audioSource;
+    private bool isFacingLeft = true;
+    private bool isGrounded = false;                        // Check is now using OnCollisionExit with Ground layer mask
+    private bool hasEnemyBelow = false;                     // See EnemyBelow() function
+    private float dismountTime = 3f;                        // Wait time when stacked on top of another enemy to unstack
+    private float dismountTimer = 0;                        // Countdown initially set to 0
+    protected bool lockMovement = false;
     protected int directionSign = 0;
 
-	[HideInInspector]
-	public EnemySpawner enemySpawner;
+    [HideInInspector]
+    public EnemySpawner enemySpawner;
 
-	protected void Awake ()
-	{
-		// Setting up references.
-		anim = GetComponentInChildren<Animator> ();
-		anim.SetFloat ("headbobSpeedMultiplier", headbobSpeedMultiplier);
-		frontCheck = transform.Find ("frontCheck").transform;
-		gameController = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController> ();
-		gameController.numberOfEnemies++;
-		playerController = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ();
-	}
+    protected void Awake()
+    {
+        // Setting up references.
+        anim = GetComponentInChildren<Animator>();
+        anim.SetFloat("headbobSpeedMultiplier", headbobSpeedMultiplier);
+        frontCheck = transform.Find("frontCheck").transform;
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        gameController.numberOfEnemies++;
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        audioSource = GetComponent<AudioSource>();
 
-	// Update is called once per frame
-	protected void Update ()
-	{
-		// Destroy object when it should despawn.
-		if (isDead) {
-			deathTimer += Time.deltaTime;
-			if (deathTimer >= deathDespawnInterval)
-				Destroy (this.gameObject);
-		}
+        // Setup variables
+        idleSoundTimer = idleSoundCountdown;
+    }
 
-		// Checks should happen in update and movements in Fixed
-		hasEnemyBelow = EnemyBelow ();
+    // Update is called once per frame
+    protected void Update()
+    {
+        // Destroy object when it should despawn.
+        if (isDead)
+        {
+            deathTimer += Time.deltaTime;
+            if (deathTimer >= deathDespawnInterval)
+                Destroy(this.gameObject);
+        }
+        else
+        {
+            idleSoundTimer -= Time.deltaTime;
 
-		// If just stacked on top of an enemy, start timer.
-		if (hasEnemyBelow && dismountTimer==0)
-			dismountTimer = dismountTime;
+            if (idleSoundTimer <= 0)
+            {
+                audioSource.clip = idleClips[Random.Range(0, idleClips.Length)];
+                if (!audioSource.isPlaying)
+                    audioSource.Play();
 
-		// Reset timer if necessary
-		if (dismountTimer < 0)
-			dismountTimer = 0;
+                idleSoundTimer = idleSoundCountdown + Random.Range(0, idleSoundRandom);
+            }
+        }
 
-	}
+        // Checks should happen in update and movements in Fixed
+        hasEnemyBelow = EnemyBelow();
 
-	void FixedUpdate ()
-	{
+        // If just stacked on top of an enemy, start timer.
+        if (hasEnemyBelow && dismountTimer == 0)
+            dismountTimer = dismountTime;
 
-		if(!lockMovement){
+        // Reset timer if necessary
+        if (dismountTimer < 0)
+            dismountTimer = 0;
 
-		directionSign = FindPlayer ();
+    }
+
+    void FixedUpdate()
+    {
+
+        if (!lockMovement)
+        {
+
+            directionSign = FindPlayer();
 
             // If stacked on an enemy cancel horizontal velocity
             if (dismountTimer > 0)
@@ -85,36 +111,37 @@ public class EnemyController : MonoBehaviour
                 // Move towards player
                 Move();
 
-		// Flip if necessary to look towards the player
-		Reorient (directionSign);
+            // Flip if necessary to look towards the player
+            Reorient(directionSign);
 
-		// The Speed animator parameter is set.
-		anim.SetFloat ("Speed", Mathf.Abs (GetComponent<Rigidbody2D> ().velocity.x));
-	}
+            // The Speed animator parameter is set.
+            anim.SetFloat("Speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+        }
 
-	}
+    }
 
-	void OnTriggerEnter2D (Collider2D col)
-	{
-		if (col.transform.tag == "DeadZone")
-			Die (3);
-	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.tag == "DeadZone")
+            Die(3);
+    }
 
-	void OnCollisionEnter2D (Collision2D col)
-	{
-		if (col.collider.gameObject.layer == LayerMask.NameToLayer ("Ground"))
-			isGrounded = true;
-	}
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            isGrounded = true;
+    }
 
 
-	void OnCollisionExit2D (Collision2D col)
-	{
-		if (col.gameObject.layer == 8
-		    && isGrounded) {
-			isGrounded = false;
-		}
-			
-	}
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.layer == 8
+            && isGrounded)
+        {
+            isGrounded = false;
+        }
+
+    }
 
     void OnParticleCollision(GameObject col)
     {
@@ -124,95 +151,100 @@ public class EnemyController : MonoBehaviour
         Die(1);
     }
 
-    public void Flip ()
-	{
-		// No flipping when dead
-		if (!isDead) {
-			// Multiply the x component of localScale by -1.
-			Vector3 enemyScale = transform.localScale;
-			enemyScale.x *= -1;
-			transform.localScale = enemyScale;
-			isFacingLeft = !isFacingLeft;
-		}
-	}
+    public void Flip()
+    {
+        // No flipping when dead
+        if (!isDead)
+        {
+            // Multiply the x component of localScale by -1.
+            Vector3 enemyScale = transform.localScale;
+            enemyScale.x *= -1;
+            transform.localScale = enemyScale;
+            isFacingLeft = !isFacingLeft;
+        }
+    }
 
-	// killType is 0 when melee, 1 when ranged, 3 when suicide
-	public void Die (int killType)
-	{
-		if (!isDead) {
+    // killType is 0 when melee, 1 when ranged, 3 when suicide
+    public void Die(int killType)
+    {
+        if (!isDead)
+        {
 
-			// Find all of the colliders on the gameobject and set them all to be triggers.
-			Collider2D[] cols = GetComponents<Collider2D> ();
-			foreach (Collider2D c in cols) {
+            // Find all of the colliders on the gameobject and set them all to be triggers.
+            Collider2D[] cols = GetComponents<Collider2D>();
+            foreach (Collider2D c in cols)
+            {
                 //c.isTrigger = true;
                 c.enabled = false;
-			}
+            }
 
 
-			// Move all sprite parts of the player to the front
-			SpriteRenderer[] spr = GetComponentsInChildren<SpriteRenderer> ();
-			foreach (SpriteRenderer s in spr) {
-				s.sortingLayerName = "UI";
-			}
+            // Move all sprite parts of the player to the front
+            SpriteRenderer[] spr = GetComponentsInChildren<SpriteRenderer>();
+            foreach (SpriteRenderer s in spr)
+            {
+                s.sortingLayerName = "UI";
+            }
 
-			// ... Trigger the 'Die' animation state
-			anim.SetTrigger ("Dead");
+            // ... Trigger the 'Die' animation state
+            anim.SetTrigger("Dead");
 
-			// Start despawning timer in update.
-			isDead = true;
+            // Start despawning timer in update.
+            isDead = true;
 
-			gameController.numberOfEnemies--;
-			if (enemySpawner != null)
-				enemySpawner.numberOfEnemies--;
+            gameController.numberOfEnemies--;
+            if (enemySpawner != null)
+                enemySpawner.numberOfEnemies--;
 
-			if (killType != 3)
-				playerController.EnemyKill (killType);
-		}
-	}
+            if (killType != 3)
+                playerController.EnemyKill(killType);
+        }
+    }
 
-	// Uses horizontal position diference to return the sign of the offset to the player.
-	protected int FindPlayer ()
-	{
-		int direction = 0;
+    // Uses horizontal position diference to return the sign of the offset to the player.
+    protected int FindPlayer()
+    {
+        int direction = 0;
 
-		Vector3 tempFloat = transform.position - playerController.transform.position;
+        Vector3 tempFloat = transform.position - playerController.transform.position;
 
-		if (tempFloat.x <= 0)
-			direction = 1;
-		else
-			direction = -1;
+        if (tempFloat.x <= 0)
+            direction = 1;
+        else
+            direction = -1;
 
-		return direction;
-	}
+        return direction;
+    }
 
-	// Flip if not facing the playe. Direction must be the sign of direction towards player.
-	private void Reorient (int direction)
-	{
-		if (direction == -1 && !isFacingLeft)
-			Flip ();
-		else if (direction == 1 && isFacingLeft)
-			Flip ();
-	}
+    // Flip if not facing the playe. Direction must be the sign of direction towards player.
+    private void Reorient(int direction)
+    {
+        if (direction == -1 && !isFacingLeft)
+            Flip();
+        else if (direction == 1 && isFacingLeft)
+            Flip();
+    }
 
     protected virtual void Move()
     {
-        if(!isDead)
-        // Set the enemy's velocity to moveSpeed in the x direction.
-        GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed * directionSign, GetComponent<Rigidbody2D>().velocity.y);
+        if (!isDead)
+            // Set the enemy's velocity to moveSpeed in the x direction.
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed * directionSign, GetComponent<Rigidbody2D>().velocity.y);
     }
 
-	// Raycast to see if stacked on top of an enemy.
-	private bool EnemyBelow(){
-		
-		GameObject bottomCheckPosition = transform.FindChild ("bottomCheck").gameObject;
-		RaycastHit2D raycastHit;
+    // Raycast to see if stacked on top of an enemy.
+    private bool EnemyBelow()
+    {
 
-		raycastHit = Physics2D.Raycast (bottomCheckPosition.transform.position, Vector2.down);
-			if (raycastHit)
-			if (raycastHit.collider.tag == "Enemy")
-				return true;
+        GameObject bottomCheckPosition = transform.FindChild("bottomCheck").gameObject;
+        RaycastHit2D raycastHit;
 
-		return false;
-	}
+        raycastHit = Physics2D.Raycast(bottomCheckPosition.transform.position, Vector2.down);
+        if (raycastHit)
+            if (raycastHit.collider.tag == "Enemy")
+                return true;
+
+        return false;
+    }
 
 }
