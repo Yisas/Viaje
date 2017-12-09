43b84ffc6cf030750fb259fccef80e5c24cbca7c
Yisas <yisas_0929@hotmail.com>
2017-12-09 10:44:32 -0500

    Underwater splash VFX

---
+++ Assets/Scripts/Misc Event Triggers/Event_Splash.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Event_Splash : MonoBehaviour {
+
+    public AudioSource audioSource;
+
+	private void OnTriggerEnter2D()
+    {
+        audioSource.Play();
+    }
+}
