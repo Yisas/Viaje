ba22e115d2dcf28e33d7834231cd06527e5edaee
Yisas <yisas_0929@hotmail.com>
2017-12-10 10:42:39 -0500

    Llovizna playlist (nelson birthday song added to loop)

---
+++ Assets/Playlist.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playlist : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip[] clips;
+    public float[] clipVolumes;
+
+    private int clipIndex = 0;
+    private int volumeIndex = 0;
+	
+	// Update is called once per frame
+	void Update () {
+        if (!audioSource.isPlaying)
+        {
+            clipIndex++;
+            volumeIndex++;
+
+            if(clipIndex > clips.Length - 1)
+            {
+                clipIndex = 0;
+            }
+
+            if (volumeIndex > clipVolumes.Length - 1)
+            {
+                volumeIndex = 0;
+            }
+
+            audioSource.clip = clips[clipIndex];
+            audioSource.volume = clipVolumes[volumeIndex];
+            audioSource.Play();
+        }
+	}
+}
