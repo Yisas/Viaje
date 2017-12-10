edc6d308782602bb79d4c8b13a6a26a8f0eb29bd
Yisas <yisas_0929@hotmail.com>
2017-12-10 11:19:50 -0500

    Monkey sounds

---
+++ Assets/RandomPlaylist.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPlaylist : Playlist
+{
+    protected override void PlayClip()
+    {
+        // Choose randomly
+        int randomIndex = Random.Range(0, clips.Length - 1);
+
+        audioSource.clip = clips[randomIndex];
+        audioSource.volume = clipVolumes[randomIndex];
+        audioSource.Play();
+    }
+}
--- Assets/Scripts/Sound/Playlist.cs
+++ Assets/Scripts/Sound/Playlist.cs
@@ -2,35 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Playlist : MonoBehaviour {
-
+public class Playlist : MonoBehaviour
+{
     public AudioSource audioSource;
     public AudioClip[] clips;
     public float[] clipVolumes;
 
-    private int clipIndex = 0;
-    private int volumeIndex = 0;
-	
-	// Update is called once per frame
-	void Update () {
+    protected int clipIndex = -1;
+    protected int volumeIndex = -1;
+
+    // Update is called once per frame
+    void Update()
+    {
         if (!audioSource.isPlaying)
         {
-            clipIndex++;
-            volumeIndex++;
-
-            if(clipIndex > clips.Length - 1)
-            {
-                clipIndex = 0;
-            }
-
-            if (volumeIndex > clipVolumes.Length - 1)
-            {
-                volumeIndex = 0;
-            }
-
-            audioSource.clip = clips[clipIndex];
-            audioSource.volume = clipVolumes[volumeIndex];
-            audioSource.Play();
+            PlayClip();
         }
-	}
+    }
+
+    protected virtual void PlayClip()
+    {
+        clipIndex++;
+        volumeIndex++;
+
+        if (clipIndex > clips.Length - 1)
+        {
+            clipIndex = 0;
+        }
+
+        if (volumeIndex > clipVolumes.Length - 1)
+        {
+            volumeIndex = 0;
+        }
+
+        audioSource.clip = clips[clipIndex];
+        audioSource.volume = clipVolumes[volumeIndex];
+        audioSource.Play();
+    }
 }
