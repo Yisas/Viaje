506ddb6cedba02c1d36301dad2bf1f6a771b3288
Yisas <yisas_0929@hotmail.com>
2017-12-10 12:40:41 -0500

    Stef greeting sound

---
+++ Assets/Scripts/Misc Event Triggers/Event_TriggerSound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event_TriggerSound : MonoBehaviour {
+
+    public AudioSource audioSource;
+
+    private bool playing = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (playing)
+            return;
+
+        if (collision.transform.tag == "Player")
+        {
+            audioSource.Play();
+
+            playing = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (playing)
+        {
+            if (!audioSource.isPlaying)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
