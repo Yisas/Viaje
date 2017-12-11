a2a4092c50930acd824c376a5d751a083c3dc21a
Yisas <yisas_0929@hotmail.com>
2017-12-10 20:54:53 -0500

    #6 Transitioning to EndScene from final level

---
+++ Assets/Scripts/Misc Event Triggers/Event_KillAllEnemies.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event_KillAllEnemies : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            EnemySpawner[] spawners = GameObject.FindObjectsOfType<EnemySpawner>();
+            foreach (EnemySpawner spawner in spawners)
+            {
+                Destroy(spawner.gameObject);
+            }
+
+            EnemyController[] enemies = GameObject.FindObjectsOfType<EnemyController>();
+            foreach(EnemyController enemy in enemies)
+            {
+                enemy.Die(3);
+            }
+        }
+    }
+}
