6a7ced9e3aa2016468e613e9d0c20469e9a62cc3
Yisas <yisas_0929@hotmail.com>
2017-12-10 20:42:58 -0500

    #6 Created End Scene

---
+++ Assets/Scripts/Misc Event Triggers/TimedSceneSwitch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedSceneSwitch : SceneSwitch {
+
+    public float countdownTime;
+
+	void Update () {
+
+        countdownTime -= Time.deltaTime;
+
+        if(countdownTime <= 0)
+        {
+            Destroy(GameController.GetInstance());
+
+            SplashCreenSwitch();
+        }
+	}
+}
