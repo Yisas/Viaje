f1c6518e855ae27dc03a8a9f6baf7d06cdc8f549
Yisas <yisas_0929@hotmail.com>
2017-12-11 00:02:13 -0500

    End scene timing update

---
+++ Assets/Scripts/Sound/TimedAudioSourcePlay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedAudioSourcePlay : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public float timerCountdown;
+
+	// Update is called once per frame
+	void Update () {
+        timerCountdown -= Time.deltaTime;
+        
+        if(timerCountdown <=0)
+        {
+            audioSource.Play();
+            Destroy(this);
+        }
+	}
+}
